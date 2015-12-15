6ecef2efc192094bf7a174df7545723c86838830
ymekuria <ymekuria@gmail.com>
2015-12-15 12:12:59 -0800

    refactored express server

--- server/expressApp.js
+++ server/expressApp.js
@@ -10,23 +10,21 @@ app.use(function(req, res, next) {
   res.header("access-control-allow-methods","GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "content-type, accept");
   res.header("access-control-max-age","10");
+  res.header('Content-Type', 'application/json');
   next();
 });
 
 app.get('/classes/messages', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(messages);
 });
 
 app.post('/classes/messages', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
   var reqObj = req.body;
   messages.addMessage(reqObj.username, reqObj.roomName, reqObj.message);
   res.status(201).json({objectId: messages.objectId});
 });
 
 app.options('/classes/messages', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
   res.status(200).send('{"status": "ok"}');
 });
 
